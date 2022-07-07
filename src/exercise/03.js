7605ebd929e65f8f55b2799151836febc22789a5
CANGARITA <CristianDaniloGutierrez.Angarita@davita.com>
2022-07-07 14:25:57 -0500

    exercises resolved from 03-06

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -20,15 +20,25 @@ function CountProvider(props) {
   )
 }
 
+function useCount() {
+  const contextValue = React.useContext(CountContext)
+
+  if (!contextValue) {
+    throw Error('useCount must be used within a CountProvider!') 
+  }
+
+  return contextValue
+}
+
 function CountDisplay() {
   // 🐨 get the count from useContext with the CountContext
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
   // 🐨 get the setCount from useContext with the CountContext
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
